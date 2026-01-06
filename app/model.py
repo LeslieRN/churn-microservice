508e6b77c9f4b63491018f8e86838a80ae640689
LeslieRN <lesliern30@hotmail.com>
2026-01-06 11:26:41 -0400

    Created new endpoint for batches request

--- app/model.py
+++ app/model.py
@@ -1,4 +1,5 @@
 from joblib import load
+import pandas as pd
 from app.schemas import ChurnInput
 
 
@@ -18,5 +19,13 @@ class ModelService:
                 "churn": prob[1]
             }
         }
+    
+    def predict_batch(self, df: pd.DataFrame) -> pd.DataFrame:
+        probs = self.model.predict_proba(df)
+        df = df.copy()
+        df["not_churn_prob"] = probs[:, 0]
+        df["churn_prob"] = probs[:, 1]
+
+        return df
 
 model_service = ModelService()
